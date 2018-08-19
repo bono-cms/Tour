bfea88331484c1e42cd2dacf829c4023d9c87522
dave-yang <daworld.ny@gmail.com>
2018-08-20 01:35:30 +0500

    Initial structure

---
+++ Module.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Tour;
+
+use Cms\AbstractCmsModule;
+
+final class Module extends AbstractCmsModule
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getServiceProviders()
+    {
+        return array(
+        );
+    }
+}
