74e12cfa67427a645accf627650f795bacba21c2
dave-yang <daworld.ny@gmail.com>
2018-08-21 02:37:15 +0500

    Added missing file-desc

--- Storage/MySQL/CategoryTranslationMapper.php
+++ Storage/MySQL/CategoryTranslationMapper.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 namespace Tour\Storage\MySQL;
 
 use Cms\Storage\MySQL\AbstractMapper;
