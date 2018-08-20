e130f949fed3395580ff0f3a01fccb07081ff39d
dave-yang <daworld.ny@gmail.com>
2018-08-21 00:40:28 +0500

    Added SEO columns

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -5,7 +5,8 @@ DROP TABLE IF EXISTS `bono_module_tour_category`;
 
 CREATE TABLE `bono_module_tour_category` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `order` INT NOT NULL COMMENT 'Sorting order'
+    `order` INT NOT NULL COMMENT 'Sorting order',
+    `seo` BOOLEAN NOT NULL COMMENT 'Whether SEO is enabled'
 );
 
 
@@ -14,6 +15,11 @@ DROP TABLE IF EXISTS `bono_module_tour_category_translation`;
 CREATE TABLE `bono_module_tour_category_translation` (
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL COMMENT 'Language identificator of this page',
+    `web_page_id` INT NOT NULL COMMENT 'Attached web-page ID',
     `name` varchar(255) NOT NULL COMMENT 'Category name',
-    `description` TEXT NOT NULL COMMENT 'Category description'
+    `description` TEXT NOT NULL COMMENT 'Category description',
+    
+    `title` varchar(255) NOT NULL COMMENT 'Page title',
+    `meta_keywords` TEXT NOT NULL COMMENT 'Keywords for search engines',
+    `meta_description` TEXT NOT NULL COMMENT 'Meta description for search engines'
 );
