b460376b90803f3f347e004b087158c0869c414d
dave-yang <daworld.ny@gmail.com>
2018-09-27 16:15:17 +0500

    Added dropper

---
+++ Storage/MySQL/Dropper.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Tour\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractStorageDropper;
+
+final class Dropper extends AbstractStorageDropper
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTables()
+    {
+        return array(
+            TourMapper::getTableName(),
+            TourTranslationMapper::getTableName(),
+            CategoryMapper::getTableName(),
+            CategoryTranslationMapper::getTableName()
+        );
+    }
+}
