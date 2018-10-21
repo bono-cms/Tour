4c73f43f682f0ea1cf8339278c7be93a915e6e93
dave-yang <daworld.ny@gmail.com>
2018-10-21 22:23:34 +0500

    Empty tour day service and its registration

---
+++ Service/TourDayService.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Tour\Service;
+
+use Tour\Storage\TourDayMapperInterface;
+
+final class TourDayService
+{
+    /**
+     * Tour day mapper
+     * 
+     * @var \Tour\Storage\TourDayMapperInterface
+     */
+    private $tourDayMapper;
+
+    /**
+     * State initialization
+     * 
+     * @param \Tour\Storage\TourDayMapperInterface $tourDayMapper
+     * @return void
+     */
+    public function __construct(TourDayMapperInterface $tourDayMapper)
+    {
+        $this->tourDayMapper = $tourDayMapper;
+    }
+}
