aa51469b42ffc46e5d24d063a22a1833b0928d6c
dave-yang <daworld.ny@gmail.com>
2018-10-22 19:51:12 +0500

    Tour gallery service implementation

--- Service/TourGalleryService.php
+++ Service/TourGalleryService.php
@@ -11,9 +11,11 @@
 
 namespace Tour\Service;
 
+use Krystal\Stdlib\VirtualEntity;
+use Cms\Service\AbstractManager;
 use Tour\Storage\TourGalleryMapperInterface;
 
-final class TourGalleryService
+final class TourGalleryService extends AbstractManager
 {
     /**
      * Any tour gallery mapper
@@ -31,5 +33,85 @@ final class TourGalleryService
     public function __construct(TourGalleryMapperInterface $tourGalleryMapper)
     {
         $this->tourGalleryMapper = $tourGalleryMapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function toEntity(array $row)
+    {
+        $image = new VirtualEntity();
+        $image->setId($row['id'])
+              ->setTourId($row['tour_id'])
+              ->setOrder($row['order'])
+              ->setImage($row['image']);
+
+        return $image;
+    }
+
+    /**
+     * Returns last image ID
+     * 
+     * @return int
+     */
+    public function getLastId()
+    {
+        return $this->tourGalleryMapper->getMaxId();
+    }
+
+    /**
+     * Fetch all images
+     * 
+     * @param int $tourId Attached tour ID
+     * @param boolean $sort Whether to sort by corresponding sorting order
+     * @return array
+     */
+    public function fetchAll($tourId, $sort)
+    {
+        return $this->prepareResults($this->tourGalleryMapper->fetchAll($tourId, $sort));
+    }
+
+    /**
+     * Fetch image by its ID
+     * 
+     * @param int $id Image ID
+     * @return mixed
+     */
+    public function fetchById($id)
+    {
+        return $this->prepareResult($this->tourGalleryMapper->findByPk($id));
+    }
+
+    /**
+     * Deletes an image by its ID
+     * 
+     * @param int $id Image ID
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->tourGalleryMapper->deleteByPk($id);
+    }
+
+    /**
+     * Adds an image
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function add(array $input)
+    {
+        return $this->tourGalleryMapper->persist($input);
+    }
+
+    /**
+     * Updates an image
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function update(array $input)
+    {
+        return $this->tourGalleryMapper->persist($input);
     }
 }
