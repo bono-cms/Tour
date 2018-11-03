f152179d46e307b491a0e338e6006418c96dbdc3
dave-yang <daworld.ny@gmail.com>
2018-11-03 22:34:10 +0500

    TourDateService implementation

--- Service/TourDateService.php
+++ Service/TourDateService.php
@@ -11,6 +11,7 @@
 
 namespace Tour\Service;
 
+use Krystal\Stdlib\VirtualEntity;
 use Cms\Service\AbstractManager;
 use Tour\Storage\TourDateMapperInterface;
 
@@ -32,5 +33,73 @@ final class TourDateService extends AbstractManager
     public function __construct(TourDateMapperInterface $tourDateMapper)
     {
         $this->tourDateMapper = $tourDateMapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function toEntity($row)
+    {
+        $entity = new VirtualEntity();
+        $entity->setStart($row['start'])
+               ->setEnd($row['end'])
+               ->setId($row['id'])
+               ->setTourId($row['tour_id']);
+
+        return $entity;
+    }
+
+    /**
+     * Delete tour date by its ID
+     * 
+     * @param int $id Tour date ID
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->tourDateMapper->deleteByPk($id);
+    }
+
+    /**
+     * Returns last ID
+     * 
+     * @return int
+     */
+    public function getLastId()
+    {
+        return $this->tourDateMapper->getMaxId();
+    }
+
+    /**
+     * Fetch tour date by its ID
+     * 
+     * @param int $id Tour date ID
+     * @return array
+     */
+    public function fetchById($id)
+    {
+        return $this->prepareResult($this->tourDateMapper->findByPk($id));
+    }
+
+    /**
+     * Fetch dates by tour ID
+     * 
+     * @param int $tourId
+     * @return array
+     */
+    public function fetchByTourId($tourId)
+    {
+        return $this->prepareResults($this->tourDateMapper->fetchByTourId($tourId));
+    }
+
+    /**
+     * Saves tour date
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function save(array $input)
+    {
+        return $this->tourDateMapper->persist($input);
     }
 }
