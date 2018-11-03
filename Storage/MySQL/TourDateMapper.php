ec6f4d2b4ac285fd6b50df969915036fa548e7b7
dave-yang <daworld.ny@gmail.com>
2018-11-03 21:22:30 +0500

    Tour date schema and its mapper

---
+++ Storage/MySQL/TourDateMapper.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Tour\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractMapper;
+use Tour\Storage\TourDateMapperInterface;
+
+final class TourDateMapper extends AbstractMapper implements TourDateMapperInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getTableName()
+    {
+        return self::getWithPrefix('bono_module_tour_dates');
+    }
+}
