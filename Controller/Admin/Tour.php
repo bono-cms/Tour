57c6204337a8ec6f94c83e93a2ff0d6a3d526a77
dave-yang <daworld.ny@gmail.com>
2018-12-13 18:05:58 +0500

    Render tour destinations

--- Controller/Admin/Tour.php
+++ Controller/Admin/Tour.php
@@ -48,7 +48,8 @@ final class Tour extends AbstractController
             'gallery' => !$new ? $this->getModuleService('tourGalleryService')->fetchAll($id, false) : array(),
             'reviews' => !$new ? $this->getModuleService('tourReviewService')->fetchAll($id, false) : array(),
             'categories' => $this->getModuleService('categoryService')->fetchList(true),
-            'tours' => $this->getModuleService('tourService')->fetchList(true)
+            'tours' => $this->getModuleService('tourService')->fetchList(true),
+            'destinations' => $this->getModuleService('tourDestinationService')->fetchList()
         ));
     }
 
