74f7adb0a74e3e3d77d9df1d4dc27802a9b44376
dave-yang <daworld.ny@gmail.com>
2018-12-13 15:39:37 +0500

    Initial TourDestination controller and its routes

---
+++ Controller/Admin/TourDestination.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Tour\Controller\Admin;
+
+use Krystal\Stdlib\VirtualEntity;
+use Cms\Controller\Admin\AbstractController;
+
+final class TourDestination extends AbstractController
+{
+    /**
+     * Create destination form
+     * 
+     * @param \Krystal\Stdlib\VirtualEntity|array $destination
+     * @return string
+     */
+    private function createForm($destination)
+    {
+        // Whether this destination is new
+        $new = is_object($destination);
+
+        // Append breadcrumbs
+        $this->view->getBreadcrumbBag()->addOne('Tours', 'Tour:Admin:Grid@indexAction')
+                                       ->addOne('Tour destinations');
+
+        return $this->view->render('tour.destination.form', array(
+            'destination' => $destination,
+            'new' => $new
+        ));
+    }
+
+    /**
+     * Renders add form
+     * 
+     * @return string
+     */
+    public function addAction()
+    {
+        return $this->createForm(new VirtualEntity);
+    }
+
+    /**
+     * Renders edit form
+     * 
+     * @param int $id Tour destination ID
+     * @return string
+     */
+    public function editAction($id)
+    {
+        $destination = $this->getModuleService('tourDestinationService')->fetchById($id, true);
+
+        if ($destination) {
+            return $this->createForm($destination);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Deletes tour destination by its ID
+     * 
+     * @param int $id Tour destination ID
+     * @return int
+     */
+    public function deleteAction($id)
+    {
+        $this->getModuleService('tourDestinationService')->deleteById($id);
+
+        $this->flashBag->set('success', 'Selected element has been removed successfully');
+        return 1;
+    }
+
+    /**
+     * Saves tour destination
+     * 
+     * @return int
+     */
+    public function saveAction()
+    {
+        // Raw request data
+        $input = $this->request->getPost();
+
+        $tourDestinationService = $this->getModuleService('tourDestinationService');
+        $tourDestinationService->save($input);
+
+        if ($input['destination']['id']) {
+            $this->flashBag->set('success', 'The element has been updated successfully');
+            return 1;
+        } else {
+            $this->flashBag->set('success', 'The element has been created successfully');
+            return $tourDestinationService->getLastId();
+        }
+    }
+}
