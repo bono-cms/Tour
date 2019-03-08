7e2ca3a0d00deb8245d8ffe6afe90e634566b8ae
dave-yang <daworld.ny@gmail.com>
2019-03-08 20:15:54 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,36 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
     'name' => 'Tour',
-    'caption' => 'Tours',
-    'route' => 'Tour:Admin:Grid@indexAction',
-    'order' => 1,
     'description' => 'Tour module lets you organize and sell tour packages on your site',
+    'menu' => array(
+        'name' => 'Tours',
+        'icon' => 'fas fa-car-side',
+        'items' => array(
+            array(
+                'route' => 'Tour:Admin:Grid@indexAction',
+                'name' => 'View all tours'
+            ),
+            array(
+                'route' => 'Tour:Admin:Tour@addAction',
+                'name' => 'Add a tour'
+            ),
+            array(
+                'route' => 'Tour:Admin:Category@addAction',
+                'name' => 'Add category'
+            ),
+            array(
+                'route' => 'Tour:Admin:Booking@indexAction',
+                'name' => 'Bookings'
+            ),
+            array(
+                'route' => 'Tour:Admin:TourDestination@indexAction',
+                'name' => 'Tour destinations'
+            )
+        )
+    )
 );
