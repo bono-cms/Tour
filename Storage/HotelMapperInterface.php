e5bc464e12f189ba304a7948d78f204ddfd3386b
dave-yang <daworld.ny@gmail.com>
2019-08-10 18:30:00 +0500

    Added fetchAll() and fetchById()

--- Storage/HotelMapperInterface.php
+++ Storage/HotelMapperInterface.php
@@ -13,4 +13,20 @@ namespace Tour\Storage;
 
 interface HotelMapperInterface
 {
+    /**
+     * Fetch all hotels
+     * 
+     * @param boolean $sort Whether to sort by corresponding sorting order
+     * @return array
+     */
+    public function fetchAll($sort);
+
+    /**
+     * Fetches a hotel by its associated id
+     * 
+     * @param string $id
+     * @param boolean $withTranslations Whether to fetch translations or not
+     * @return array
+     */
+    public function fetchById($id, $withTranslations);
 }
--- Storage/MySQL/HotelMapper.php
+++ Storage/MySQL/HotelMapper.php
@@ -11,6 +11,7 @@
 
 namespace Tour\Storage\MySQL;
 
+use Krystal\Db\Sql\RawSqlFragment;
 use Cms\Storage\MySQL\AbstractMapper;
 use Tour\Storage\HotelMapperInterface;
 
@@ -30,5 +31,63 @@ final class HotelMapper extends AbstractMapper implements HotelMapperInterface
     public static function getTranslationTable()
     {
         return HotelTranslationMapper::getTableName();
+    }
+
+    /**
+     * Returns shared columns to be selected
+     * 
+     * @return array
+     */
+    private function getColumns()
+    {
+        return array(
+            self::column('id'),
+            self::column('order'),
+            HotelTranslationMapper::column('web_page_id'),
+            HotelTranslationMapper::column('lang_id'),
+            HotelTranslationMapper::column('name'),
+            HotelTranslationMapper::column('description'),
+            HotelTranslationMapper::column('title'),
+            HotelTranslationMapper::column('meta_keywords'),
+            HotelTranslationMapper::column('meta_description'),
+        );
+    }
+
+    /**
+     * Fetch all hotels
+     * 
+     * @param boolean $sort Whether to sort by corresponding sorting order
+     * @return array
+     */
+    public function fetchAll($sort)
+    {
+        $db = $this->createWebPageSelect($this->getColumns())
+                   // Language ID constraint
+                   ->whereEquals(HotelTranslationMapper::column('lang_id'), $this->getLangId());
+
+        if ($sort === false) {
+            // Sort by last IDs
+            $db->orderBy(self::column('id'))
+               ->desc();
+        } else {
+            $db->orderBy(array(
+                self::column('order'), 
+                new RawSqlFragment(sprintf('CASE WHEN %s = 0 THEN %s END DESC', self::column('order'), self::column('id')))
+            ));
+        }
+
+        return $db->queryAll();
+    }
+
+    /**
+     * Fetches a hotel by its associated id
+     * 
+     * @param string $id
+     * @param boolean $withTranslations Whether to fetch translations or not
+     * @return array
+     */
+    public function fetchById($id, $withTranslations)
+    {
+        return $this->findWebPage($this->getColumns(), $id, $withTranslations);
     }
 }
