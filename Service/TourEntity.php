097657c3fd646f0e178d5c2863dd631d6e93d014
dave-yang <daworld.ny@gmail.com>
2019-11-25 13:55:46 +0500

    Added hasConstraintLanguageId() and isConstraintLanguageId()

--- Service/TourEntity.php
+++ Service/TourEntity.php
@@ -16,6 +16,33 @@ use Krystal\Stdlib\VirtualEntity;
 final class TourEntity extends VirtualEntity
 {
     /**
+     * Checks whether current tour entity has language constraint
+     * 
+     * @return boolean
+     */
+    public function hasConstraintLanguageId()
+    {
+        return (bool) $this->getLangConstraintId();
+    }
+
+    /**
+     * Checks whether target language id is a constraint one
+     * 
+     * @param int $langId
+     * @return boolean
+     */
+    public function isConstraintLanguageId($langId)
+    {
+        $constraintId = $this->getLangConstraintId();
+
+        if ($constraintId == 0) {
+            return false;
+        }
+
+        return $langId == $constraintId;
+    }
+
+    /**
      * Returns count of available nights
      * 
      * @return integer
