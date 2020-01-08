04138b0e3658b19bc3b227bef3f9953f7125b793
dave-yang <daworld.ny@gmail.com>
2020-01-08 21:09:07 +0500

    Fixed issue with breadcrumbs

--- Controller/Admin/TourDate.php
+++ Controller/Admin/TourDate.php
@@ -19,25 +19,18 @@ final class TourDate extends AbstractController
     /**
      * Renders form
      * 
-     * @param mixed $date Tour date
+     * @param \Krystal\Stdlib\VirtualEntity $date Tour date
      * @return string
      */
-    private function createForm($date)
+    private function createForm(VirtualEntity $date)
     {
         $tour = $this->getModuleService('tourService')->fetchById($date->getTourId(), false);
 
         if ($tour !== false) {
-            // Grab ID
-            if (is_array($date)) {
-                $id = $date[0]['tour_id'];
-            } else {
-                $id = $date->getTourId();
-            }
-
             // Append breadcrumbs
             $this->view->getBreadcrumbBag()->addOne('Tours', 'Tour:Admin:Grid@indexAction')
-                                           ->addOne($this->translator->translate('Edit the tour "%s"', $tour->getName()), $this->createUrl('Tour:Admin:Tour@editAction', array($id)))
-                                           ->addOne(!is_array($date) ? 'Add new date' : 'Edit the date');
+                                           ->addOne($this->translator->translate('Edit the tour "%s"', $tour->getName()), $this->createUrl('Tour:Admin:Tour@editAction', array($date->getTourId())))
+                                           ->addOne(!$date->getId() ? 'Add new date' : 'Edit the date');
             // Load plugins
             $this->view->getPluginBag()
                        ->load('datepicker');
